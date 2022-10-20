b3f29833dd73a67a0e75a153abdd4e471e849ad1
Julian da Costa Faro <juliandacostafaro@gmail.com>
2022-10-19 22:53:40 -0300

    mark X and O alternatively

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 
 
 function Square(props) {
-     return (
+    return (
         <button
             className="square"
             onClick={props.onClick}
@@ -21,13 +21,20 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState(
+            {
+                squares: squares,
+                xIsNext: !this.state.xIsNext,
+            }
+        )
     }
 
     renderSquare(i) {
@@ -38,7 +45,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' +  (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
